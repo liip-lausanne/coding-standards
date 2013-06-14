3e09a96ed425df089245d90089270cbd09ff8b7f
Jeanmonod David <david.jeanmonod@gmail.com>
2013-06-14 15:36:16 +0200

    Add js code example

---
+++ js/code.js
@@ -0,0 +1,14 @@
+var hashMap = {
+    'keys_with_snake_case' = 'toto',
+    ...
+}
+
+// jQuery object use the $ perfix
+var $link = $('a.my-link');
+
+
+var object = {
+    '_internalMethod' = function() {
+        console.log('Method name who start by an underscore should usually not be used');
+    };
+}
