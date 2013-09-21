4bb7f9eb29ce837f36a8d634deb85857a7173d5d
Gilles Meier <krtek4@gmail.com>
2013-09-21 22:18:01 +0200

    Update PHP coding style
    inspired by PSR-0, PSR-1 and PSR-2

--- php/code.php
+++ php/code.php
@@ -1,7 +1,42 @@
+<?php
 
-$hashMap = array(
-   'keys_in_snake_case' => 1
-   ...
-);
+namespace Some\Namespace;
 
-return $this->render('MyBundle:twig_template_are_named_with_snake_case.html.twig', array());
+class CamelCaseClass
+{
+    const SOME_CONSTANT = "a value";
+
+    /**
+     * Function are always documented with at least parameters type and
+     * return type. If possible, type is enforced through PHP (array, class)
+     *
+     * @return string bla bla bla
+     */
+    private function camelCaseFunction(array $elements)
+    {
+        foreach($elements as $k => $e) {
+            if($e == 'something') {
+                echo "found something";
+            } else {
+                $someData = $e;
+                break;
+            }
+        }
+        return $someData;
+    }
+    
+    public function render()
+    {
+        $values = array(
+            'keys_in_snake_case' => 1
+        );
+
+        return $this->render('MyBundle:twig_template_are_named_with_snake_case.html.twig', $values);
+    }
+    
+    final protected static function someStaticFunction()
+    {
+       // do something
+    }
+}
+
