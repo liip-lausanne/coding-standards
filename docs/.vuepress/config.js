f1d3a500a6d59d6367be6855d134fa76ed8f9fc5
Benoît Burgener <benoit.burgener@liip.ch>
2018-12-01 17:57:55 +0100

    Fix VuePress base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Liip LS front-end standards',
+  base: '/coding-standards/',
   themeConfig: {
-    base: '/coding-standards/',
     repo: 'team-rawbot/coding-standards',
     repoLabel: 'GitHub',
     lastUpdated: 'Last Updated',
